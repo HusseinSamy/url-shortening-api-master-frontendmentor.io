5e3828685e369f2b047829787b0521670cf6c4af
Hussein Samy <hussein.samy02@gmail.com>
2023-03-19 04:43:51 +0200

    Adds main javascript for dynamic styling

---
+++ main.js
@@ -0,0 +1,160 @@
+//////////////////////////////// 
+//Change background of the shorten link section dynamically
+//////////////////////////////// 
+    const shortenLinkContainer = document.getElementById("shorten-link-container")
+    const boostAreaContainer = document.getElementById("boost-area-container")
+    function changeBackground() {
+        if (window.innerWidth < 1120) {
+            shortenLinkContainer.classList.add(['shorten-link-background-mobile'])
+            boostAreaContainer.classList.add(['boost-background-mobile'])
+        }
+        else {
+            shortenLinkContainer.classList.remove(['shorten-link-background-mobile'])
+            boostAreaContainer.classList.remove(['boost-background-mobile'])
+        }
+    }
+    changeBackground();
+    window.onresize = changeBackground;
+////////////////////////////////
+////////////////////////////////
+
+
+
+//////////////////////////////// 
+// Burger menu logic
+//////////////////////////////// 
+    const burgerMenu = document.getElementById("burger-menu")
+    burgerMenu.addEventListener("click", () => {
+        console.log('clicked');
+        const mainMenu = document.getElementById("main-menu");
+        console.log(mainMenu);
+        mainMenu.classList.toggle("toggle-display")
+    })
+////////////////////////////////
+////////////////////////////////
+
+
+
+
+////////////////////////////////
+// Shrtco api integration
+////////////////////////////////
+    const shortenButton = document.getElementById("shorten-button")
+    shortenButton.addEventListener("click", async (event) => {
+        const linkToShorten = document.getElementById("link-to-shorten").value;
+        // Calls shortening link API
+        try {
+            const result = await callShortenAPI(linkToShorten);
+            // Add new component after shortening
+            appendNewLinkComponent(linkToShorten, result.data.result.short_link, result.data.result.code);
+        }
+        catch (err) {
+            alert(err)
+        }
+    })
+
+
+
+///////////////// Helper functions /////////////////
+
+        // Appends new component for the resulting link
+        function appendNewLinkComponent(originalLink, shortenedLink, id) {
+            const linksParent = document.getElementById("section-2");
+
+            const linkContainer = document.createElement('div');
+            linkContainer.classList.add('shortened-link-container')
+            
+        // Creates the left part of the component
+            const leftLinkEl = document.createElement('div');
+            leftLinkEl.classList.add('shortened-left')
+            const leftlinkContentEl = document.createElement('p');
+            leftlinkContentEl.textContent = originalLink
+            leftLinkEl.appendChild(leftlinkContentEl);
+            linkContainer.appendChild(leftLinkEl)
+
+        // Creates the rigth part of the component
+            const rightLinkEl = document.createElement('div');
+            rightLinkEl.classList.add('shortened-right')
+            const rightlinkContentParent = document.createElement('div');
+            const rightlinkContent = document.createElement('p');
+            rightlinkContent.textContent = `https://${shortenedLink}`
+            rightlinkContentParent.appendChild(rightlinkContent);
+            const rightlinkButtonParent = document.createElement('div');
+            const rightlinkButton = document.createElement('button');
+            rightlinkButton.id = id
+            rightlinkButton.textContent = 'Copy';
+            rightlinkButtonParent.appendChild(rightlinkButton)
+
+            rightLinkEl.appendChild(rightlinkContentParent);
+            rightLinkEl.appendChild(rightlinkButtonParent);
+            linkContainer.appendChild(rightLinkEl)
+            linksParent.appendChild(linkContainer)
+            addCallBackToCopyButton(id, `https://${shortenedLink}`)
+        }
+
+        // Adds call back to all copy buttons
+        function addCallBackToCopyButton(id, value) {
+            const button = document.getElementById(id);
+            button.addEventListener('click', () => {
+                navigator.clipboard.writeText(value)
+                .then(() => {
+                    button.classList.add('copied-link')
+                    button.innerText = 'Copied!';
+                })
+                .catch(() => {
+                    Alert('Failed to copy!');
+                });
+            });
+        }
+
+        // Calls shortening links api
+        async function callShortenAPI(link) {
+            const result = await axios({
+                method: 'get',
+                baseURL: 'https://api.shrtco.de/v2',
+                url:'/shorten',
+                params: {
+                    url: link
+                }
+            });
+            return result;
+        }
+////////////////////////////////
+////////////////////////////////
+
+
+////////////////////////////////
+// Input validation dynamic styling
+////////////////////////////////
+
+    let inputClickCounter = 0;
+    const inputField = document.getElementById("link-to-shorten")
+    const validationError = document.getElementById("validation-error")
+    // Counts numbers of clicks on the input
+    inputField.addEventListener('click', () => {
+        inputClickCounter++;
+    })
+
+    // Checks if the input is clicked (toched) and doesn't contain value 
+    inputField.addEventListener('focusin', () => {
+        if (inputClickCounter > 0 && inputField.value == "") {
+        inputField.classList.add('invalid-input')
+        }
+        else {
+            inputField.classList.remove('invalid-input');
+        }
+    })
+    inputField.addEventListener('focusout', () => {
+        if (inputClickCounter > 0 && inputField.value == "") {
+            inputField.classList.add('invalid-input');
+            window.innerWidth > 1120 ?
+                validationError.classList.add('toggle-display'): console.log();
+        }
+        else {
+            inputField.classList.remove('invalid-input');
+            window.innerWidth > 1120 ?
+                validationError.classList.remove('toggle-display'): console.log();
+        }
+    })
+////////////////////////////////
+////////////////////////////////
